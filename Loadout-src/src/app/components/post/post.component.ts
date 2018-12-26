e5e17064de713f8cd70b0bdf8d0d5a21fb14d5b9
mckiernantim <mckiernantim@gmail.com>
2018-12-26 13:46:06 +0100

    built out JSON data for final

--- Loadout-src/src/app/components/post/post.component.ts
+++ Loadout-src/src/app/components/post/post.component.ts
@@ -28,14 +28,17 @@ export class PostComponent implements OnInit {
   claimedBy: String;
   specifics: String;
   selections = postOptions;
-  currentData: any = {
-    firstSelection: null,
-    secondSelection: null,
-
-  }
-  currentOptions: any[]
+   currentOptions: String[]
+  thirdOptions: String[]
+  fourthOptions: String[]
   showSecond: boolean = null
-  showThird:boolean= false;
+  showThird:boolean= false
+  showFourth:boolean = false
+  currentDataName:String
+  counter:number = 0
+  subcatIndex: number = 0
+  thirdCatIndex: number =0
+  fourthCatIndex:number = 0
 
 
 
@@ -43,19 +46,7 @@ export class PostComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
     private postService: PostService) {
-    // this.rForm = fb.group({
-    //   'title': [null, Validators.required],
-    //   'description': [null, Validators.compose([Validators.required, Validators.minLength(5)])],
-    //   'category': [null, Validators.required],
-    //   "subCategory": [null, Validators.required],
-    //   "item": [null, Validators.required],
-    //   'deathDate': [null, Validators.required],
-    //   'specifics': [null],
-    //   'validate': '',
-
-
-
-    // })
+  
   }
   getSubcategory(selectedSubCat: string) {
     if (selectedSubCat) {
@@ -75,12 +66,8 @@ export class PostComponent implements OnInit {
       title: this.title,
       deathDate: this.deathDate.toString(),
       specifics: this.specifics
-      
-
-
-
-    }
-    console.log(post.description + " ************")
+     }
+    console.log(post)
     this.postService.createPost(post).subscribe(data => {
       console.log(data)
     })
@@ -95,49 +82,154 @@ export class PostComponent implements OnInit {
 
 
   }
-  toggleCategory(event) {
-    if(!this.currentData.firstSelection){
-    this.currentData.firstSelection = event.target.value}
-    else{
-      if(!this.currentData.secondSelection){this.currentData.secondSelection = event.target.value}
+  toggleCategory(event, num) {
+    
+    this.counter++;
+    console.log(this.counter + "  !!!!!!!!!!!!  COUNTER INCREASED !!!!!!!!!!!!!!!!")
+
+    
+    
+    this.toggleDisplay(this.counter)
+    let selectedTitle = (event.target.value);
+    console.log(selectedTitle)
+    this.currentDataName = event.target.getAttribute('id');
+   
+    if (this.counter === 1  ){
+       
+      let returnedData = this.displaySubcategory(selectedTitle, this.currentDataName);
+      this.currentOptions = returnedData.options;
+      this.subcatIndex = returnedData.index
+      console.log(this.subcatIndex + " subcategory selected index!!!!" + selectedTitle)
+    
+    } 
+    
+    if (this.counter === 2){
+     
+      
+      let returnedData = this.displayThirdCategory(selectedTitle, this.currentDataName, this.subcatIndex);
+     
+      this.thirdOptions = returnedData.options;
+      console.log(this.thirdOptions + " ppppppppppppp")
+      this.thirdCatIndex = returnedData.index;
+     
       
     }
-   
-    // let selectedStr = (event.target.value);
-    // console.log(event.target.getAttribute('name'))
-    // this.currentName = event.target.getAttribute('name')
-    // if(!this.showSecond || this.showSecond && !this.showThird){
-    // this.currentOptions = this.displaySubcategory(selectedStr, this.currentName);}
 
-
-    // console.log(this.currentOptions + " <------- your current options");
-}
+    if (this.counter === 3 ){
+      
+      let returnedData = this.displayFourthCategory(selectedTitle, this.currentDataName, this.thirdCatIndex);
+      this.fourthOptions = returnedData.options;
+      console.log(this.fourthOptions)
+      
+      this.fourthCatIndex = returnedData.index;
+      
+    }
+    if (this.counter === 4 ) {
+      
+    }
  
+   
+    
+
+    console.log(this.currentOptions + " current options")
+    console.log(this.thirdOptions + " third options")
+
+    ;
+}
   displaySubcategory(str: any, dataToDisplay: String) {
+  
     let childData = {
       index: 0,
       options: [],
     };
-    for (let i = 0; i <= this.selections.length; i++) {
-      console.log(str)
-      console.log(this.selections[i].department)
-
+    for (let i = 0; i < this.selections.length; i++) {
+      console.log(this.selections.length)
+      console.log(str, this.selections[i].department  +  "  ********")
       if (str === this.selections[i].department) {
-        
-        childData.index = i;
+         childData.index = i;
         for (let j = 0; j < this.selections[i].children.length; j++) {
-          childData.options.push(this.selections[i][`${dataToDisplay}`][j].title)
-          console.log(childData.options)
-         if(!this.showSecond)
-           {this.showSecond = true;}
+          childData.options.push(this.selections[i][`${dataToDisplay}`][j].subDepartment)
+          
         }
       }
-      return childData.options;
+     
+    } return childData;
+  }
+ 
+  displayThirdCategory(str:any, previousCategory: String, index: number){
+   
+   
+    let childData = {
+      index: 0,
+      options: [],
+    };
+     
+    for (let i = 0; i < this.selections[this.subcatIndex].children.length; i++) {
+     
+    
+      if (str === this.selections[this.subcatIndex].children[i].subDepartment) {
+       console.log(this.selections[this.subcatIndex].children[i].subDepartment + " this is the current title being iterated")
+         childData.index = i;
+        for (let j = 0; j < this.selections[this.subcatIndex].children.length; j++) {
+          console.log(previousCategory + " this is the data we will return !!!!!!!", this.subcatIndex + " <-------this.subCatIndex" , i+ " <----- I")
+          console.log(this.counter)
+          childData.options.push(this.selections[this.subcatIndex].children[i][`${previousCategory}`][j].title)
+        
+          
+        }
+      }
     }
+      return childData;
+   }
+   displayFourthCategory(str:any, dataToDisplay: String, index: number){
+   
+  
+    let childData = {
+      index: 0,
+      options: [],
+    };
+      
+    for (let i = 0; i < this.selections[this.subcatIndex].children[this.thirdCatIndex].subDepartment.length; i++) {
+     
+     
+      if (str === this.selections[this.subcatIndex].children[i].subDepartment) {
+       
+         childData.index = i;
+        for (let j = 0; j < this.selections[this.subcatIndex].children[this.thirdCatIndex].subDepartment.length; j++) {
+          childData.options.push(this.selections[this.subcatIndex].children[this.thirdCatIndex].subDepartment[i][`${dataToDisplay}`][j].itle)
+         
+          
+        }
+      }
+    }
+      return childData;
+   }
+
+ 
+  
+  
+  
+  
+  
+  
+  
+  toggleDisplay(num){
+    if (num === 1 ){this.showSecond = true}
+    console.log( this.showSecond + " showing second")
+    if(num ===2   ) {this.showThird = true }
+    console.log (this.showThird + " showing third")
+    if (num=== 3  ) {this.showFourth =true }
+    
+  }
+
   }
 
 
-}
+ 
+ 
+
+
+
 
 
 
--- Loadout-src/src/app/components/post/postOptions.ts
+++ Loadout-src/src/app/components/post/postOptions.ts
@@ -1,113 +1,236 @@
-export const postOptions =  [{
+export const postOptions = [
+  {
 
-        department: "electrical",
-        children: [
+    department: "electrical",
+    children: [
+      {
+        subDepartment: "instruments",
+        options: [
+
           {
-             title: "instruments",
-            subcategories: [
-    
-              {
-                subTitle: "Source4",
-                specifics: "description"
-              },
-              {
-                subTitle: "Fresnel",
-                specifics: "description"
-    
-              },
-              {
-                subTitle: "Pars",
-                specifics: "description"
-    
-              },
-              {
-                subTitle: "Strips",
-                specifics: "description"
-    
-              },
-              {
-                subTitle: "Other",
-                specifics: "description"
-    
-              },
-            ]
-          },
-    
-          {
-            title: "cables",
-            subcategories: [
-              {
-                subTitle: "edison",
-                specifics: ["specify length"]
-              },
-              {
-                subTitle: "stage pin",
-                specifics: ["specify length"]
-              },
-    
-              {
-                subTitle: "dmx",
-                specifics: ["specify length, specify pin"]
-              },
-    
-              {
-                subTitle: "other",
-                specifics: ["length"]
-              },
-    
-            ]
+            subTitle: "Source4",
+            specifics: "description"
           },
           {
-            title: "hardware",
-            subcategories: [
-              {
-                subTitle: "Cheesegoroughs",
-                specifics: [{
-                  options: [{
-                    type: "swivel",
-                    materials: ["steel", "aluminum"]
-                  },
-                  {
-                    type: "Rigid",
-                    materials: ["steel", "aluminum"]
-                  },
-                  {
-                    type: "HalfBoro",
-                    materials: ["steel", "aluminum"]
-                  },
-                  ],
-                }]
-    
+            subTitle: "Fresnel",
+            specifics: "description"
+
+          },
+          {
+            subTitle: "Pars",
+            specifics: "description"
+
+          },
+          {
+            subTitle: "Strips",
+            specifics: "description"
+
+          },
+          {
+            subTitle: "Other",
+            specifics: "description"
+
+          },
+        ]
+      },
+
+      {
+        subDepartment: "cables",
+        subcategories: [
+          {
+            subTitle: "edison",
+            specifics: ["enter", "length"]
+          },
+          {
+            subTitle: "stage pin",
+            specifics: ["enter", "length"]
+          },
+
+          {
+            subTitle: "dmx",
+            specifics: ["enter", "length, enter", "pin"]
+          },
+
+          {
+            subTitle: "other",
+            specifics: ["length"]
+          },
+
+        ]
+      },
+      {
+        subDepartment: "hardware",
+        subcategories: [
+          {
+            subTitle: "Cheesegoroughs",
+            specifics: [{
+              subOption: [{
+                type: "swivel",
+                materials: ["steel", "aluminum"]
               },
               {
-                subTitle: 'schedule40',
-                specifics: ["specify length"]
+                type: "Rigid",
+                materials: ["steel", "aluminum"]
               },
               {
-                subTitle: "boomBase",
-                specifics: []
+                type: "HalfBoro",
+                materials: ["steel", "aluminum"]
+              },
+              ],
+            }]
+
+          },
+          {
+            subTitle: 'schedule40',
+            specifics: ["enter", "length"]
+          },
+          {
+            subTitle: "boomBase",
+            specifics: []
+          },
+          {
+            subTitle: "sideArms",
+            specifics: []
+          },
+          {
+            subTitle: "cClamps",
+            specifics: []
+          },
+          {
+            subTitle: "safetyCables",
+            specifics: []
+          },
+          {
+            subTitle: "other",
+            specifics: []
+          },
+
+        ]
+
+      }
+    ]
+  },
+  {
+    department: "scenery",
+    children: [
+      {
+        subDepartment: "materials",
+
+        subcategories: [{
+          title: "lumber",
+          options: [
+            {
+              itemTile: "sheet",
+              subOptions: [{
+                optionTitle: "masonite",
+                optionSpecifics: ["enter", "2x4 specifics", "other specifics"]
               },
               {
-                subTitle: "sideArms",
-                specifics: []
+                optionTitle: "plywood",
+                optionSpecifics: ["enter", "4x8 specifics",]
+              }]
+            },
+            {
+              itemTitle: "stick",
+              subOptions: [{
+                optionTitle: "2x4",
+                optionSpecifics: ["enter", "whether painted", "length"]
               },
               {
-                subTitle: "cClamps",
-                specifics: []
-              },
-              {
-                subTitle: "safetyCables",
-                specifics: []
-              },
-              {
-                subTitle: "other",
-                specifics: []
-              },
-    
-            ]
-    
-          }
+                optionTitle: "other",
+                optionSpecifics: ["enter", "whether painted", "length", "description"]
+              }]
+            },
+          ]
+        },
+
+        {
+          title: "hardware",
+          options: [{
+            itemTile: "",
+            subOptions: [{
+              optionTitle: "",
+              optionSpecifics: []
+            }]
+          }]
+        },
+        {
+          title: "soft goods",
+          options: [{
+            itemTile: "",
+            subOptions: [{
+              optionTitle: "",
+              optionSpecifics: []
+            }]
+          }]
+        },
+        {
+          title: "paint",
+          options: [{
+            itemTile: "",
+            subOptions: [{
+              optionTitle: "",
+              optionSpecifics: []
+            }]
+          }]
+        }]
+      },
+
+
+      {
+        subDepartment: "built/bought",
+        subcategories: [{
+          title: 'flats (1 by fram, 1/4" luan)',
+          options: [{
+            itemTile: "",
+            subOptions: [{
+              optionTitle: "",
+              optionSpecifics: []
+            }]
+          }]
+        },
+        {
+          title: "doors",
+          options: [{
+            itemTile: "",
+            subOptions: [{
+              optionTitle: "",
+              optionSpecifics: []
+            }]
+          }]
+        },
+        {
+          title: "decking/flooring",
+          options: [{
+            itemTile: "",
+            subOptions: [{
+              optionTitle: "",
+              optionSpecifics: []
+            }]
+          }]
+        },
+        {
+          title: "stairs",
+          options: [{
+            itemTile: "",
+            subOptions: [{
+              optionTitle: "",
+              optionSpecifics: []
+            }]
+          }]
+        },
+        {
+          title: "soft goods",
+          options: [{
+            itemTile: "",
+            subOptions: [{
+              optionTitle: "",
+              optionSpecifics: []
+            }]
+          }]
+        }
+
         ]
       }
     ]
-
+  }]
